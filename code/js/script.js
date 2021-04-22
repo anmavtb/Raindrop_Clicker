33e7c0b67f72ea7dd1ac23b31e2a7c055b4c3919
anmavtb <vincent.guiraudou@gmail.com>
2021-04-22 16:36:42 +0200

    automatisation des méthodes

--- code/js/script.js
+++ code/js/script.js
@@ -1,33 +1,41 @@
-let dropsAmount = 0,
-    maxWaterLevel = 10,
-    money = 0,
+let dropsAmount = 1000,
+    maxWaterLevel = 1000,
+    money = 10000,
     moneyMultiplier = 2,
     priceTank = 10,
     charNumber = 0;
 
 let possibleVillagers = [["villager_1.gif", 10], ["villager_2.gif", 25], ["villager_3.gif", 75]];
 let allVillagers = [];
+let villagerRank = [];
+let clone;
+
+let elem = document.getElementById("animVillager");
+
+let id = null;
+let pos = 0;
+let maxPos = 600;
 
 let Villager = function (image, demand) {
-    this.image = "images/" + image;
+    this.image = image;
     this.demand = demand;
     this.money = (Math.round(this.demand * moneyMultiplier));
-    this.walk = function () {
-        let id = null;
-        let elem = document.getElementById("animVillager");
-        elem.image = this.image;
-        let pos = 0;
+    this.walk = function (clone) {
         clearInterval(id);
         id = setInterval(frame, 10);
         function frame() {
-            if (pos == 500) {
+            if (pos == maxPos) {
                 clearInterval(id);
+                maxPos -= 100;
+                if (pos == 600) {
+                    getWater(allVillagers);
+                }
             } else {
-                pos+=5;
-                elem.style.left = pos + 'px';
+                pos += 5;
+                clone.style.left = pos + 'px';
             }
         }
-    };
+    }
 }
 
 function update() {
@@ -50,8 +58,7 @@ function getWater(allVillagers) {
         if (actualVillager.demand !== 0 && dropsAmount >= actualVillager.demand) {
             dropsAmount -= actualVillager.demand;
             money += actualVillager.money;
-            allVillagers.splice(0, 1);
-            console.log(allVillagers);
+            suprVillager(allVillagers);
             update();
         } else {
             console.log(`Pas assez d'eau, il faut au moins ${actualVillager.demand} gouttes.`);
@@ -62,27 +69,50 @@ function getWater(allVillagers) {
 
 };
 
-function createVillager() {
-    if (charNumber == undefined) {
-        charNumber = 0;
+function createVillager(allVillagers) {
+    pos = 0;
+    if (maxPos >= 100) {
+        let thisVillager = randomVillager();
+        allVillagers.push(new Villager(thisVillager[0], thisVillager[1]));
+        clone = document.createElement("div");
+        clone.style.width = "100px";
+        clone.style.height = "100px";
+        clone.style.position = "absolute";
+        clone.style.backgroundImage = "url('images/" + thisVillager[0] + "')";
+        clone.style.backgroundPosition = "center";
+        clone.style.backgroundSize = "cover";
+        elem.appendChild(clone);
+        allVillagers[charNumber].walk(clone);
+        charNumber++;
+    } else {
+        console.log("Pas de place");
     }
-    let thisVillager = randomVillager();
-    allVillagers.push(new Villager(thisVillager[0], thisVillager[1]));
-    console.log(allVillagers);
-    allVillagers[charNumber].walk();
-    charNumber++;
 };
 
 function randomVillager() {
-    min = 0;
-    max = possibleVillagers.length - 1;
-    let randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    let randomNumber = Math.floor(Math.random() * possibleVillagers.length);
     return (possibleVillagers[randomNumber]);
 };
 
 function suprVillager(allVillagers) {
     allVillagers.splice(0, 1);
-    console.log(allVillagers);
+    charNumber--;
+    elem.firstChild.remove();
+    console.log(elem.childNodes.length);
+    maxPos = 600;
+    for (let i = 0; i < elem.childNodes.length; i++) {
+        console.log("--[" + i + "]---");
+        console.log(elem.childNodes[i]);
+        pos = parseInt(elem.childNodes[i].style.left);
+        console.log(pos, maxPos);
+        while (elem.childNodes[i].style.left != (maxPos + "px")) {
+            pos += 5;
+            elem.childNodes[i].style.left = pos + 'px';
+        }
+        maxPos -= 100;
+        console.log(elem.childNodes[i].style.left);
+        console.log("--[" + i + "]---");
+    }
 }
 
 function upgradeTank() {
